9eed9890fad441208fe6bf27bbaae2ada3afaea4
rooooose <57755204+rooooose@users.noreply.github.com>
2019-11-16 00:44:43 +0100

    Header page principale

---
+++ html/headerPrincipal.php
@@ -0,0 +1,20 @@
+<header>
+	<div class="wrap">
+		<div class="topBar">
+			<a href="index.php">
+				<img src="../images/Logo.svg" alt="Logo Rope Up!">
+			</a>
+			<nav>
+				<a href="#" class="navContent">Qui sommes-nous ?</a>
+				<a href="#" class="navContent">Domaines</a>
+				<a href="#" class="navContent">Contact</a>
+				<a href="#"><span>Connexion</span></a>
+			</nav>
+		</div>	
+	</div>
+	<div class="imgContainer">
+		<span>L'outil de Communication et de Gestion des professionnels indépendants</span>
+		<img src="../images/Img left.jpg" alt="clavier">
+		<img src="../images/Img right.jpg" alt="moitié homme">
+	</div>
+</header>
---
+++ html/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+	<link rel="stylesheet" type="text/css" href="../css/libs/reset.css">
+	<link rel="stylesheet" type="text/css" href="../css/headerPrincipal.css">
+	<link rel="stylesheet" type="text/css" href="../css/fonts.css">
+	<title>Page Principale Rope Up!</title>
+</head>
+<body>
+	<!--inclure header -->
+	<?php include("headerPrincipal.php") ?>
+</body>
+</html>
