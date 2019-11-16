07fb4de8692cc092f44aee47b46637b9391a0a12
rooooose <57755204+rooooose@users.noreply.github.com>
2019-11-16 12:01:11 +0100

    modifs liens et autres détails

--- html/headerMenu.php
+++ html/headerMenu.php
@@ -1,13 +1,13 @@
 <header>
 	<div class="headerMenu">
 		<div class="wrap">
-			<a href="index.php">
+			<a href="pagePrincipale.php">
 				<img src="../images/Logo.svg" alt="Logo Rope Up!">
 			</a>
 			<nav>
-				<a href="#" class="navContent">Qui sommes-nous ?</a>
+				<a href="quiSommesNous.php" class="navContent">Qui sommes-nous ?</a>
 				<a href="#" class="navContent">Domaines</a>
-				<a href="#" class="navContent">Contact</a>
+				<a href="contact.php" class="navContent">Contact</a>
 				<a href="#"><span>Connexion</span></a>
 			</nav>	
 		</div>
