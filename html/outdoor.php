38a7e6716c559d2bed520cf0526e9f854b054864
michelgege <50527053+michelgege@users.noreply.github.com>
2019-11-17 12:28:53 +0100

    outdoor 50% complete

--- html/outdoor.php
+++ html/outdoor.php
@@ -7,17 +7,115 @@
 	<link rel="stylesheet" type="text/css" href="../css/libs/reset.css">
 	<link rel="stylesheet" type="text/css" href="../css/headerPrincipal.css">
 	<link rel="stylesheet" type="text/css" href="../css/headerOutdoor.css">
+	<link rel="stylesheet" type="text/css" href="../css/outdoor.css">
 	<link rel="stylesheet" type="text/css" href="../css/footer.css">
 	<link rel="stylesheet" type="text/css" href="../css/fonts.css">
 	<title>Outdoor</title>
 </head>
-<body>
+<body> 
 	<!--inclure header -->
 		<?php include("headerMenu.php") ?>
 		<div class="imgContainer">
 			<h1>Outdoor</h1>
 		</div>
 	</header>
+
+	<section id="presentation">
+			<h2>Ce que Rope Up vous propose</h2>
+
+			<div>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget enim et turpis consequat commodo id sit amet purus. Quisque et lobortis sem. Phasellus in volutpat erat. Mauris varius pharetra viverra. Nam consectetur iaculis congue. Nulla tincidunt malesuada cursus. Nam faucibus cursus velit in viverra. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas convallis ut ex cursus sollicitudin. Duis vitae faucibus quam. Aenean aliquet nisi eget erat gravida, eget cursus lacus auctor. Proin eu maximus tortor. Sed et semper lacus, sit amet porttitor massa. Vivamus tincidunt erat vel sem iaculis, lacinia ultricies erat dignissim.
+				</p>	
+			</div>
+	</section>
+
+	<section id="fonctionnalités">
+
+		<div class="pack_container">
+			<div class="management">
+				<p>
+					pack gestion
+				</p>
+
+			</div>
+
+			<div class="communication">
+				<p>
+					pack communication
+				</p>
+			</div>
+		</div>
+		
+			
+		<div class="background">
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget enim et turpis consequat commodo id sit amet purus. Quisque et lobortis sem. Phasellus in volutpat erat. Mauris varius pharetra viverra. Nam consectetur iaculis congue. Nulla tincidunt malesuada cursus. Nam faucibus cursus velit in viverra. Lorem ipsum dolor sit amet, consectetur.
+			</p>
+
+
+
+			<div class="flex_container">
+
+				<div class="video_and_description">
+					<video src="../videos/yaah.mp4"></video>
+					<p></p>
+					
+				</div>
+
+				<div class="functionalities">
+					<ul>
+						<li>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit.  
+						</li>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+							Nullam eget enim et turpis consequat commodo id sit 
+							amet purus. Quisque et lobortis sem. Phasellus in 
+							volutpat erat. 
+						</p>
+
+						<li>
+							Quisque et lobortis sem. Phasellus in volutpat erat. 
+						</li>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+							Nullam eget enim et turpis consequat commodo id sit 
+							amet purus. Quisque et lobortis sem. Phasellus in 
+							volutpat erat.
+						</p>
+
+						<li>
+							Quisque et lobortis sem. Phasellus in volutpat erat. 
+						</li>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+							Nullam eget enim et turpis consequat commodo id sit 
+							amet purus. Quisque et lobortis sem. Phasellus in 
+							volutpat erat. 
+						</p>
+					</ul>
+				</div>
+			</div>
+
+
+
+
+
+
+		</div> <!-- background -->
+
+
+
+
+
+
+
+
+
+
+	</section>
+
 
 	<?php include("footer.php") ?>
 </body>
