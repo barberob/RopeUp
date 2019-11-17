ba6045616b128f25474893a432f64a5c4033a7b1
rooooose <57755204+rooooose@users.noreply.github.com>
2019-11-17 13:12:28 +0100

    noms de pages et images changés

--- html/espaces_verts.php
+++ html/espaces_verts.php
@@ -5,15 +5,15 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
 	<link rel="stylesheet" type="text/css" href="../css/libs/reset.css">
-	<link rel="stylesheet" type="text/css" href="../css/headerPrincipal.css">
-	<link rel="stylesheet" type="text/css" href="../css/headerEspacesVerts.css">
+	<link rel="stylesheet" type="text/css" href="../css/header_principal.css">
+	<link rel="stylesheet" type="text/css" href="../css/header_espaces_verts.css">
 	<link rel="stylesheet" type="text/css" href="../css/footer.css">
 	<link rel="stylesheet" type="text/css" href="../css/fonts.css">
 	<title>Espaces Verts</title>
 </head>
 <body>
 	<!--inclure header -->
-		<?php include("headerMenu.php") ?>
+		<?php include("header_menu.php") ?>
 		<div class="imgContainer">
 			<h1>Espaces Verts</h1>
 		</div>
--- html/qui_sommes_nous.php
+++ html/qui_sommes_nous.php
@@ -5,15 +5,15 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
 	<link rel="stylesheet" type="text/css" href="../css/libs/reset.css">
-	<link rel="stylesheet" type="text/css" href="../css/headerPrincipal.css">
-	<link rel="stylesheet" type="text/css" href="../css/headerQuiSommesNous.css">
+	<link rel="stylesheet" type="text/css" href="../css/header_principal.css">
+	<link rel="stylesheet" type="text/css" href="../css/header_qui_sommes_nous.css">
 	<link rel="stylesheet" type="text/css" href="../css/footer.css">
 	<link rel="stylesheet" type="text/css" href="../css/fonts.css">
 	<title>Qui Sommes-Nous ?</title>
 </head>
 <body>
 	<!--inclure header -->
-		<?php include("headerMenu.php") ?>
+		<?php include("header_menu.php") ?>
 		<div class="imgContainer">
 			<h1>Qui sommes-nous ?</h1>
 		</div>
