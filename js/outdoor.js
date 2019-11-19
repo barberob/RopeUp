79dc0680d32bd450decd2e908dcfd748f21e2569
michelgege <50527053+michelgege@users.noreply.github.com>
2019-11-19 18:54:28 +0100

    added hover on buttons

--- js/outdoor.js
+++ js/outdoor.js
@@ -3,15 +3,15 @@ $(document).ready(function()
 {
 
 
-	$(".communication").click(function()
+	$(".communication,.communication h3").click(function()
 	{
-		$('.background:nth-of-type(2)').addClass('translated');
+		$('.background:nth-of-type(2)').addClass('translated_right');
 		$('.triangle_down').addClass('translated_triangle');
 	});
 
 	$(".management").click(function()
 	{
-		$('.background:nth-of-type(2)').removeClass('translated');
+		$('.background:nth-of-type(2)').removeClass('translated_right');
 		$('.triangle_down').removeClass('translated_triangle');
 	});
 
