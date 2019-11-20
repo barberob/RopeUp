cac0a1c4772fd4907692e5f49b30e48aa4a5f0dd
thejojo974 <johanndumoulin734@gmail.com>
2019-11-20 08:30:08 +0100

    Petits changements

--- html/index.php
+++ html/index.php
@@ -4,15 +4,15 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 	<title>Document</title>
-	<link rel="stylesheet" href="css/libs/reset.css">
-	<link rel="stylesheet" href="css/index.css">
+	<link rel="stylesheet" href="../css/libs/reset.css">
+	<link rel="stylesheet" href="../css/index.css">
 </head>
 <body>
 	<!-- Header -->
 	<header>
 		<div>
-			<img src="img/img_left.jpg" alt="Photo rapproché clavier d'ordinateur">
-			<img src="img/img_right.jpg" alt="Photo visage homme">
+			<img src="imgages/img_left.jpg" alt="Photo rapproché clavier d'ordinateur">
+			<img src="imgages/img_right.jpg" alt="Photo visage homme">
 		</div>
 		<div>
 			<h1>L'outil de Communication et de Gestion des professionnels indépendants</h1>
@@ -31,7 +31,7 @@
 			Aenean aliquet nisi eget erat gravida, eget cursus lacus auctor. Proin eu maximus tortor. 
 			Sed et semper lacus, sit amet porttitor massa. Vivamus tincidunt erat vel sem iaculis, 
 			lacinia ultricies erat dignissim.</p>
-			<img src="img/logo.png" alt="Logo RopeUp">
+			<img src="imgages/logo.png" alt="Logo RopeUp">
 		</div>
 	</section>
 
@@ -41,12 +41,12 @@
 		<div class="wrap">
 			<div>
 				<h3>Gestion</h3>
-				<img src="img/calendar.svg" alt="Logo calendrier">
+				<img src="imgages/calendar.svg" alt="Logo calendrier">
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget enim et turpis consequat commodo id sit amet purus. Quisque et lobortis sem. Phasellus in volutpat erat. Mauris varius pharetra viverra. Nam consectetur iaculis congue. Nulla tincidunt malesuada cursus. Nam faucibus cursus velit in viverra. </p>
 			</div>
 			<div>
 				<h3>Communication</h3>
-				<img src="img/social_care.svg" alt="Logo communication">
+				<img src="imgages/social_care.svg" alt="Logo communication">
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget enim et turpis consequat commodo id sit amet purus. Quisque et lobortis sem. Phasellus in volutpat erat. Mauris varius pharetra viverra. Nam consectetur iaculis congue. Nulla tincidunt malesuada cursus. Nam faucibus cursus velit in viverra. </p>
 			</div>
 		</div>
@@ -59,11 +59,11 @@
 		<p>Les produits de Rope Up! sont adaptés à votre domaine,<br>veuillez choisir le votre :</p>
 		<div id="slider">
 			<div>
-				<img src="img/slider1.jpg" alt="Photo Outdoor">
+				<img src="imgages/slider1.jpg" alt="Photo Outdoor">
 				<p>OUTDOOR</p>
 			</div>
 			<div>
-				<img src="img/slider2_2.jpg" alt="Photo Espaces verts">
+				<img src="imgages/slider2_2.jpg" alt="Photo Espaces verts">
 				<p>ESPACES VERTS</p>
 			</div>
 		</div>
@@ -74,18 +74,18 @@
 		<h2>NOS PARTENAIRES</h2>
 		<div class="wrap">
 			<div>
-				<img src="img/part_1.png" alt="Logo partenaire ...">
-				<img src="img/part_2.png" alt="Logo partenaire ...">
-				<img src="img/part_3.png" alt="Logo partenaire ...">
-				<img src="img/part_4.png" alt="Logo partenaire ...">
-				<img src="img/part_5.png" alt="Logo partenaire ...">
-				<img src="img/part_6.png" alt="Logo partenaire ...">
+				<img src="imgages/part_1.png" alt="Logo partenaire ...">
+				<img src="imgages/part_2.png" alt="Logo partenaire ...">
+				<img src="imgages/part_3.png" alt="Logo partenaire ...">
+				<img src="imgages/part_4.png" alt="Logo partenaire ...">
+				<img src="imgages/part_5.png" alt="Logo partenaire ...">
+				<img src="imgages/part_6.png" alt="Logo partenaire ...">
 			</div>
 		</div>
 		
 	</section>
 
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-	<script type="text/javascript" src="js/index.js"></script>
+	<script type="text/javascript" src="../js/index.js"></script>
 </body>
 </html>
