0fdfcfa5e10354d3a1c85743c37c0006ebffd005
rooooose <57755204+rooooose@users.noreply.github.com>
2019-11-21 22:39:50 +0100

    Create page_principale.php

---
+++ html/page_principale.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+	<link rel="stylesheet" type="text/css" href="../css/libs/reset.css">
+	<link rel="stylesheet" type="text/css" href="../css/header_principal.css">
+	<link rel="stylesheet" type="text/css" href="../css/footer.css">
+	<link rel="stylesheet" type="text/css" href="../css/fonts.css">
+	<link rel="stylesheet" type="text/css" href="../css/header_responsive.css">
+	<link rel="stylesheet" type="text/css" href="../css/footer_responsive.css">
+	<title>Page Principale Rope Up!</title>
+</head>
+<body>
+	<!--inclure header -->
+		<?php include("header_menu.php") ?>
+		<div class="img_container">
+			<span>L'outil de Communication et de Gestion des professionnels indépendants</span>
+		</div>
+	</header>
+
+	<?php include("footer.php") ?>
+
+	<script type="text/javascript" src="../js/libs/jquery.js"></script>
+	<script type="text/javascript" src="../js/menu.js"></script>
+</body>
+</html>
