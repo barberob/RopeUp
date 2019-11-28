7fef7d1931c1a1cbce5d5aeec4f07c7ee7f891fc
paulliiiine <57755343+paulliiiine@users.noreply.github.com>
2019-11-28 17:09:26 +0100

    text

--- html/qui_sommes_nous.php
+++ html/qui_sommes_nous.php
@@ -30,12 +30,15 @@
 	<section>
 		<div class="wrap_qsn">
 			<p>
-			Nous sommes 
-			L’application vient du terrain, elle répond à la demande des professionnels indépendants. 
-			Ceux qui ne font pas du business mais qui veulent vivre de leur métier passion, ne trouvent pas d’outil de gestion pertinent  ! 
+			Nous sommes une équipe de passionés qui croient en l'importance de soutenir le <span> professionel indépendant</span> dans la gestion de son activité.
+			 
 			Vivant depuis des années dans l'environnement de la montagne, notre équipe l'a bien compris. 
 			Elle est convaincue qu'elle a sa pierre à apporter à l'édifice. Les indépendants vont trouver en Rope Up ! une solution qui sait leur parler et qui les aide à rester centrés dans leur coeur de métier. 
-			L'écologie humaine assurera à tous un avenir durable. </p>
+			L'écologie humaine assurera à tous un avenir durable. 
+
+			L’application vient du<span> terrain,</span> elle répond à la demande des professionnels. 
+			Ceux qui ne font pas du business mais qui veulent vivre de leur <span>métier passion</span> et ne trouvent pas d’outil de gestion pertinent  !
+		</p>
 			<img src="../images/cabane_ropeup.svg" alt="Logo RopeUp">
 		</div>
 	</section>
