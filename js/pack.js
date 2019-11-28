03ad282334705533d7cc89956b0b18e63d6cd6dd
michelgege <50527053+michelgege@users.noreply.github.com>
2019-11-28 12:53:45 +0100

    iframe

--- js/pack.js
+++ js/pack.js
@@ -4,10 +4,10 @@ $(document).ready(function()
 
     // var video1 = $('iframe:nth-of-type(1)')[0];
     // var video2 = $('iframe:nth-of-type(2)')[0];
-       
+
 
 	$(".communication,.communication h3").click(function()
-	{
+	{    console.log('yaaa');
 		$('.background.com').removeClass('hided');
 		$('.background.manage').addClass('hided');
 		$('.triangle_down').addClass('translated_triangle');
